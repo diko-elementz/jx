a8940729dbddfc2c28beb20e16affe183ee86ca0
Diko <diko316@gmail.com>
2015-07-26 02:45:07 +0800

    - partially created builder and setting TODO comment in the build file

---
+++ tools/build.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * TODO:
+ *    1. lint - execute lint.sh
+ *    2. list all modules to memory
+ *    3. sequentially do the following to listed module
+ *       3.1 parse and get required modules and queue them to runner
+ *       3.2 queue current module to runner
+ *       3.3 run the runner and apply the following to each module
+ *          3.3.1 generate module container with Jx.inline
+ *          3.3.2 append source code with current module
+ *          3.3.3 close module container
+ */
