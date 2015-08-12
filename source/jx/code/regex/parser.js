46b795976f82f70916b6c8d54a9b293cfe85c2e9
Dexter John Consunji <diko316@gmail.com>
2015-08-13 00:35:30 +0800

    - fixed bug in tokenize() of jxCodeRegexParser where token buffer is not in sync with token queue producing truncated '.' operator or sometimes no ender token '$' returned. - created jxCodeRegexParser unit tests

--- source/jx/code/regex/parser.js
+++ source/jx/code/regex/parser.js
@@ -1,298 +1,350 @@
 
 Jx('jxClass', function (Class) {
 
-   var NOT_HEX = /[^0-9a-f]/i,
-      CTRL_CHAR_SEQUENCE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-      CTRL_CHAR = {
-            '0': '\0',
-            'n': '\n',
-            'r': '\r',
-            't': '\t',
-            'b': '\b',
-            'f': '\f'
-         },
-      OPERAND_START_TOKENS = {
-            'literal': true,
-            'ref': true,
-            '[]': true,
-            '[^]': true,
-            'wildcard': true,
-            '?': true,
-            '*': true,
-            '+': true,
-            '{}': true,
-            ')': true
-         },
-      OPERAND_END_TOKENS = {
-            'literal': true,
-            'ref': true,
-            '[]': true,
-            '[^]': true,
-            'wildcard': true,
-            '[': true,
-            '(': true
-         },
-      OPERATOR_PRECEDENCE = {
-            '.': 1,
-            '|': 2,
-            '?': 3,
-            '*': 3,
-            '+': 3,
-            '{}': 3,
-            '(': 4
+    var NOT_HEX_RE = /[^0-9a-f]/i,
+        RANGE_RE = /^([0-9]+|[0-9]*\,[0-9]+|[0-9]+\,[0-9]*)$/,
+        CTRL_CHAR_SEQUENCE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        CTRL_CHAR = {
+                '0': '\0',
+                'n': '\n',
+                'r': '\r',
+                't': '\t',
+                'b': '\b',
+                'f': '\f'
+            },
+        OPERAND_START_TOKENS = {
+                'literal': true,
+                'ref': true,
+                '[]': true,
+                '[^]': true,
+                'wildcard': true,
+                '?': true,
+                '*': true,
+                '+': true,
+                '{}': true,
+                ')': true
+            },
+        OPERAND_END_TOKENS = {
+                'literal': true,
+                'ref': true,
+                '[]': true,
+                '[^]': true,
+                'wildcard': true,
+                '[': true,
+                '(': true
+            },
+        OPERATOR_PRECEDENCE = {
+                '.': 1,
+                '|': 2,
+                '?': 3,
+                '*': 3,
+                '+': 3,
+                '{}': 3
+            };
 
-         };
+    function escapeFrom(at, subject) {
+        var S = String,
+            chr = subject.charAt(at++),
+            len = 1,
+            str = '';
+        var index;
+        switch (chr) {
+        // control
+        case 'c':
+            chr = subject.charAt(at++).toUpperCase();
+            index = CTRL_CHAR_SEQUENCE.indexOf(chr);
+            if (index == -1) {
+                str = 'c';
+            }
+            else {
+                str = S.fromCharCode(index + 1);
+                len++;
+            }
+            break;
+        case '0':
+        case 'n':
+        case 'r':
+        case 't':
+        case 'b':
+        case 'f':
+            str = CTRL_CHAR[at];
+            break;
 
-   function escapeFrom(at, subject) {
-      var S = String,
-         chr = subject.charAt(at++),
-         len = 1,
-         str = '';
-      var index;
-      switch (chr) {
-      // control
-      case 'c':
-         chr = subject.charAt(at++).toUpperCase();
-         index = CTRL_CHAR_SEQUENCE.indexOf(chr);
-         if (index == -1) {
-            str = 'c';
-         }
-         else {
-            str = S.fromCharCode(index + 1);
-            len++;
-         }
-         break;
-      case '0':
-      case 'n':
-      case 'r':
-      case 't':
-      case 'b':
-      case 'f':
-         str = CTRL_CHAR[at];
-         break;
+        // hex
+        case 'x':
+            chr = subject.substring(at, at + 2);
+            len = 3;
+            if (NOT_HEX_RE.test(chr)) {
+                len = 0;
+            }
+            else {
+                str = S.fromCharCode(parseInt(chr, 16));
+            }
+            break;
+        // utf-8
+        case 'u':
+            chr = subject.substring(at, at + 4);
+            len = 5;
+            if (NOT_HEX_RE.test(chr)) {
+                len = 0;
+            }
+            else {
+                str = S.fromCharCode(parseInt(chr, 16));
+            }
+            break;
+        // escaped literal
+        default:
+            str = chr;
+        }
+        return len ? [str, len] : false;
+    }
 
-      // hex
-      case 'x':
-         chr = subject.substring(at, at + 2);
-         len = 3;
-         if (NOT_HEX.test(chr)) {
-            len = 0;
-         }
-         else {
-            str = S.fromCharCode(parseInt(chr, 16));
-         }
-         break;
-      // utf-8
-      case 'u':
-         chr = subject.substring(at, at + 4);
-         len = 5;
-         if (NOT_HEX.test(chr)) {
-            len = 0;
-         }
-         else {
-            str = S.fromCharCode(parseInt(chr, 16));
-         }
-         break;
-      // escaped literal
-      default:
-         str = chr;
-      }
-      return len ? [str, len] : false;
-   }
+    this.exports = Class.extend({
 
-   this.exports = Class.extend({
+        subject: '',
+        index: 0,
+        parseIndex: 0,
+        ended: false,
+        parseEnded: false,
+        tokens: void(0),
+        buffer: void(0),
 
-      subject: '',
-      index: 0,
-      parseIndex: 0,
-      ended: false,
-      tokens: void(0),
-      buffer: void(0),
+        stack: void(0),
+        queue: void(0),
 
-      constructor: function (subject) {
-         if (subject instanceof RegExp) {
-            subject = subject.source;
-         }
-         if (typeof subject == 'string') {
-            this.subject = subject;
-         }
-         this.tokens = [];
-         this.buffer = [];
-      },
+        constructor: function (subject) {
+           if (subject instanceof RegExp) {
+              subject = subject.source;
+           }
+           if (typeof subject == 'string') {
+              this.subject = subject;
+           }
+           this.tokens = [];
+           this.buffer = [];
+        },
 
-      tokenize: function () {
-         var tokens = this.tokens,
-            tl = tokens.length,
-            buffer = this.buffer,
-            bl = buffer.length;
+        tokenize: function () {
+            var tokens = this.tokens,
+                tl = tokens.length,
+                buffer = this.buffer,
+                bl = buffer.length;
 
-         var subject, chr, str, o, token, strlen, index, len, l, strs, stl, to;
+            var subject, chr, str, o, token, strlen, index,
+                len, l, strs, stl, to;
 
-         if (bl) {
+            if (bl) {
 
-            o = buffer[--bl];
-            buffer.length = bl;
-            return o;
+                return tokens[tl++] = buffer.shift();
 
-         }
-         else if (!this.ended) { // tokenize
-            subject = this.subject;
-            len = subject.length;
-            index = this.index;
-            token = null;
-            strlen = 1;
-            chr = subject.charAt(index++);
+            }
+            else if (!this.ended) { // tokenize
+                subject = this.subject;
+                len = subject.length;
+                index = this.index;
+                token = null;
+                strlen = 1;
+                chr = subject.charAt(index++);
 
-            failed: switch (chr) {
-            // escape?
-            case '\\':
-               o = escapeFrom(index, subject);
-               if (o) {
-                  token = 'literal';
-                  str = o[0];
-                  strlen += o[1];
-               }
-               else {
-                  strlen = 0;
-               }
-               break;
-
-            // operators
-            case '|':
-
-            case '?':
-            case '*':
-            case '+':
-
-            case '(':
-            case ')':
-               str = token = chr;
-               break;
-
-            // character sets
-            case '[':
-               token = '[]';
-               if (subject.charAt(index + 1) == '^') {
-                  token = '[^]';
-                  index++;
-                  strlen++;
-               }
-
-               strs = [];
-               stl = 0;
-
-               loop: for (l = len - index; l--;) {
-                  chr = subject.charAt(index++);
-                  strlen ++;
-                  switch (chr) {
-                  case '\\':
-                     o = escapeFrom(index, subject);
-                     if (o) {
-                        strs[stl++] = o[0];
-                        to = o[1];
-                        strlen += to;
-                        index += to;
-                        l -= to;
-                     }
-                     else {
+                failed: switch (chr) {
+                // escape?
+                case '\\':
+                    o = escapeFrom(index, subject);
+                    if (o) {
+                        token = 'literal';
+                        str = o[0];
+                        strlen += o[1];
+                    }
+                    else {
                         strlen = 0;
-                        token = null;
-                        break failed;
-                     }
-                     break;
-                  case ']':
-                     break loop;
-                  default:
-                     strs[stl++] = chr;
-                  }
-               }
+                    }
+                    break;
 
-               str = strs.join('');
-               break;
+                // operators
+                case '|':
 
-            // character range
-            case '{':
-               break;
-            // special chars
-            case '.':
-               token = 'wildcard';
-               str = chr;
-               break;
-            case '^':
-               token = 'start';
-               str = chr;
-               break;
-            case '$':
-               token = 'end';
-               str = chr;
-               break;
+                case '?':
+                case '*':
+                case '+':
 
-            // literals
-            default:
-               token = 'literal';
-               str = chr;
-               break;
+                case '(':
+                case ')':
+                    str = token = chr;
+                    break;
+
+                // character sets
+                case '[':
+                    token = '[]';
+                    if (subject.charAt(index + 1) == '^') {
+                        token = '[^]';
+                        index++;
+                        strlen++;
+                    }
+
+                    strs = [];
+                    stl = 0;
+
+                    loop: for (l = len - index; l--;) {
+                        chr = subject.charAt(index++);
+                        strlen ++;
+                        switch (chr) {
+                        case '\\':
+                            o = escapeFrom(index, subject);
+                            if (!o) {
+                                strlen = 0;
+                                break failed;
+                            }
+                            strs[stl++] = o[0];
+                            to = o[1];
+                            strlen += to;
+                            index += to;
+                            l -= to;
+                            break;
+                        case ']':
+                            break loop;
+                        default:
+                            strs[stl++] = chr;
+                        }
+                    }
+
+                    str = strs.join('');
+                    break;
+
+                // character range
+                case '{':
+                    strs = [];
+                    stl = 0;
+
+                    loop: for (l = len - index; l--;) {
+                        chr = subject.charAt(index++);
+                        strlen++;
+                        switch (chr) {
+                        case '\\':
+                            o = escapeFrom(index, subject);
+                            if (!o) {
+                                strlen = 0;
+                                break failed;
+                            }
+                            strs[stl++] = o[0];
+                            to = o[1];
+                            strlen += to;
+                            index += to;
+                            l -= to;
+                            break;
+                        case '}':
+                            break loop;
+                        default:
+                            strs[stl++] = chr;
+                        }
+                    }
+                    str = strs.join('');
+                    token = RANGE_RE.test(str) ? '{}' : 'ref';
+                    break;
+                // special chars
+                case '.':
+                    token = 'wildcard';
+                    str = chr;
+                    break;
+                case '^':
+                    token = 'start';
+                    str = chr;
+                    break;
+                case '$':
+                    token = 'end';
+                    str = chr;
+                    break;
+
+                // literals
+                default:
+                    token = 'literal';
+                    str = chr;
+                    break;
+                }
+
+                if (strlen) {
+                    l = this.index;
+                    this.index += strlen;
+                    this.ended = this.index == len;
+
+                    if (tl) { // append concat to buffer
+                        o = tokens[tl - 1];
+
+                        if (o[0] in OPERAND_START_TOKENS &&
+                            token in OPERAND_END_TOKENS) {
+                            buffer[bl++] = [token, str];
+                            token = str = '.';
+                        }
+
+                    }
+
+                    if (this.ended) { // append $ token to buffer
+                        buffer[bl++] = ['$', ''];
+                    }
+
+                    return tokens[tl++] = [token, str];
+                }
+
+                this.ended = true;
+                return false;
             }
 
-            if (token) {
-               l = this.index;
-               this.index += strlen;
-               this.ended = this.index == len;
+            return void(0);
 
-               if (tl) { // append concat to buffer
-                  o = tokens[tl - 1];
+        },
 
-                  if (o[0] in OPERAND_START_TOKENS &&
-                     token in OPERAND_END_TOKENS) {
-                     tokens[tl++] = o = ['.', '.'];
-                     tokens[tl++] = buffer[bl++] = [token, str];
-                     return o;
-                  }
+        next: function () {
+            var P = OPERATOR_PRECEDENCE,
+                stack = this.stack,
+                queue = this.queue,
+                result = this.tokenize();
 
-               }
+            var token, value, precedence, sl, ql, s;
 
-               if (this.ended) { // append $ token to buffer
+            if (result) {
+                sl = stack.length;
+                ql = queue.length;
+                token = result[0];
 
-                  tokens[tl++] = buffer[bl++] = ['$', ''];
+                switch (name) {
+                case '.':
+                case '|':
+                case '?':
+                case '*':
+                case '+':
+                case '{}':
+                    precedence = OPERATOR_PRECEDENCE[name];
+                    if (sl) {
+                        s = stack[sl - 1];
 
-               }
 
-               return tokens[tl++] = [token, str];
+
+                    }
+                    else {
+                        this.parseEnded = true;
+                    }
+                    break;
+                default:
+                    break;
+                }
             }
-            this.ended = true;
-            return false;
-         }
 
-         return void(0);
+            return void(0);
+        },
 
-      },
+        reset: function () {
+            var list = this.buffer;
+            list.splice(0, list.length);
+            list = this.tokens;
+            list.splice(0, list.length);
 
-      next: function () {
+            list = this.stack;
+            list.splice(0, list.length);
+            list = this.queue;
+            list.splice(0, list.length);
+            delete this.index;
+            delete this.parseIndex;
+            delete this.ended;
+        }
 
-         var token, name;
-
-         for (; token = this.tokenize();) {
-            name = token[0];
-            console.log(name, ' = ', token[1]);
-            switch (name) {
-
-            }
-         }
-
-         return void(0);
-
-      },
-
-      reset: function () {
-         var list = this.buffer;
-         list.splice(0, list.length);
-         list = this.tokens;
-         list.splice(0, list.length);
-         delete this.index;
-         delete this.parseIndex;
-         delete this.ended;
-      }
-
-   });
+    });
 
 });
--- source/tests/jx/code/regex/parser.js
+++ source/tests/jx/code/regex/parser.js
@@ -1,31 +1,112 @@
 describe("jxCodeRegexParser module", function() {
    var J = Jx;
-   var SuperClass, SubClass, myInstance, mySubInstance;
 
-   Jx.setBaseUrl('/base/');
+   J.setBaseUrl('/base/');
 
-   it('can tokenize', function (done) {
-      Jx.use('jxClass', function (Class) {
-         SuperClass = Class.extend(Object, {
-               name: 'superClass',
-               overriddenName: 'superClass',
-               items: undefined,
-               constructor: function () {
-                  this.items = [];
-               }
-            });
+   it('can tokenize Regular Expression and auto append concatenate operator ' +
+      'to resulting tokens',
+      function (done) {
+         J.use('jxCodeRegexParser', function (Parser) {
+            var subject = /abc\,d\xf2(ef)/,
+               parser = new Parser(subject),
+               tokens = [
+                  ['literal', 'a'],
+                  ['.', '.'],
+                  ['literal', 'b'],
+                  ['.', '.'],
+                  ['literal', 'c'],
+                  ['.', '.'],
+                  ['literal', ','],
+                  ['.', '.'],
+                  ['literal', 'd'],
+                  ['.', '.'],
+                  ['literal', '\xf2'],
+                  ['.', '.'],
+                  ['(', '('],
+                  ['literal', 'e'],
+                  ['.', '.'],
+                  ['literal', 'f'],
+                  [')', ')'],
+                  ['$', '']
+               ],
+               c = 0;
 
-         myInstance = new SuperClass;
+            var token, match;
 
-         expect(Jx.isFunction(SuperClass)).toBe(true);
+            for (; token = parser.tokenize(); c++) {
+               match = tokens[c];
+               expect(token[0]).toBe(match[0]);
+               expect(token[1]).toBe(match[1]);
+            }
 
-         expect(SuperClass.prototype.$superclass).toBe(Object.prototype);
-
-         expect(Jx.isArray(myInstance.items)).toBe(true);
-
-         done();
+            expect(token !== false).toBe(true);
+            done();
+         });
       });
-   });
+
+
+   it('can tokenize Regular Expression by escaping characters ' +
+      'in Character Class',
+      function (done) {
+         J.use('jxCodeRegexParser', function (Parser) {
+            var subject = /[\u00f1\xf1]/,
+               parser = new Parser(subject),
+               tokens = [
+                  ['[]', '\u00f1\xf1'],
+                  ['$', '']
+               ],
+               c = 0;
+
+            var token, match;
+
+            for (; token = parser.tokenize(); c++) {
+               match = tokens[c];
+               expect(token[0]).toBe(match[0]);
+               expect(token[1]).toBe(match[1]);
+            }
+
+            expect(token !== false).toBe(true);
+            done();
+         });
+      });
+
+   it('can tokenize Regular Expression and determine a Repeater Range ' +
+      'from a Named Reference',
+      function (done) {
+         J.use('jxCodeRegexParser', function (Parser) {
+            var subject = /a{,1}b{10,}c{20}e{Name}{Delimiter}/,
+               parser = new Parser(subject),
+               tokens = [
+                  ['literal', 'a'],
+                  ['{}', ',1'],
+                  ['.', '.'],
+                  ['literal', 'b'],
+                  ['{}', '10,'],
+                  ['.', '.'],
+                  ['literal', 'c'],
+                  ['{}', '20'],
+                  ['.', '.'],
+                  ['literal', 'e'],
+                  ['.', '.'],
+                  ['ref', 'Name'],
+                  ['.', '.'],
+                  ['ref', 'Delimiter'],
+                  ['$', '']
+               ],
+               c = 0;
+
+            var token, match;
+
+            for (; token = parser.tokenize(); c++) {
+               match = tokens[c];
+               expect(token[0]).toBe(match[0]);
+               expect(token[1]).toBe(match[1]);
+            }
+
+            expect(token !== false).toBe(true);
+            done();
+         });
+      });
 
 
 });
